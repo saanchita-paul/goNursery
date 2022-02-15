118c92e150de063ca73dd52661a85f20377b18ce
Sanchita Paul <68769067+saanchita-paul@users.noreply.github.com>
2022-02-16 00:24:54 +0600

    update

--- common/footer.php
+++ common/footer.php
@@ -32,33 +32,9 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12 text-center">
-                    <p><span class="En">&copy;</span> <?php echo fEn2Bn(date("Y")); ?> | <a href="<?php echo $sSiteURL; ?>">®</a> All rights reserved by <?php echo $sInitialBN; ?></p>
+                    <p><span class="En">&copy;</span> 2022 | <a href="<?php echo $sSiteURL; ?>">®</a> All rights reserved by <?php echo $sInitialBN; ?></p>
                 </div>
             </div>
         </div>
     </div>
 </footer>
-
-<!-- <section>
-    <div class="container-fluid">
-        <div class="DScroll">
-            <div class="DScrollSection">
-                <div class="ScrollHeading d-flex justify-content-center">
-                    <p>শীর্ষ সংবাদ:</p>
-                </div>
-                <div class="ScrollSubject">
-                    <marquee onmouseover="this.stop();" onmouseout="this.start();">
-                        <a href=""><span><div class="SquareIcon"></div> বিশ্বজুড়ে করোনায় মৃত্যু ৪৯ লাখ ছাড়ালো</span></a>
-                        <a href=""><span><div class="SquareIcon"></div> পীরগঞ্জে হিন্দু পল্লীতে আগুনের ঘটনায় ৪১ জন গ্রেপ্তার</span></a>
-                        <a href=""><span><div class="SquareIcon"></div> অসাম্প্রদায়িক চেতনায় বাংলাদেশ গড়তে চাই ॥ প্রধানমন্ত্রী</span></a>
-                        <a href=""><span><div class="SquareIcon"></div> বাংলাদেশকে ১৪১ রানের লক্ষ্য ছুড়ে দিলো স্কটল্যান্ড</span></a>
-                        <a href=""><span><div class="SquareIcon"></div> মুহিবুল্লাহ হত্যাকান্ডে জড়িত সন্দেহে আরসা ক্যাডার গ্রেফতার</span></a>
-                        <a href=""><span><div class="SquareIcon"></div> সামাজিক মাধ্যমে গুজব রটনাকারীদের বিরুদ্ধে ব্যবস্থা নিচ্ছে পুলিশ</span></a>
-                        <a href=""><span><div class="SquareIcon"></div> ভারতের দুই রাজ্যে ভারী বৃষ্টির প্রভাবে ৮৫ জনের প্রাণহানি</span></a>                 
-                    </marquee>
-                </div>
-                
-            </div>
-        </div>
-    </div>
-</section> -->
